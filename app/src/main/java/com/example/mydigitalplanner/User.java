170611995236d377df7dab571464d80cc38da4e6
th8887 <th8887@bs.amalnet.k12.il>
2021-12-16 15:01:02 +0200

    User class and register xml.

---
+++ app/src/main/java/com/example/mydigitalplanner/User.java
@@ -0,0 +1,56 @@
+package com.example.mydigitalplanner;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private int phone;
+    private String uID;
+    private ArrayList<String> category;
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getuID() {
+        return uID;
+    }
+
+    public ArrayList<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(ArrayList<String> category) {
+        this.category = category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public void setuID(String uID) {
+        this.uID = uID;
+    }
+
+    public void addCategory(String s){
+        category.add(s);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", phone=" + phone +
+                ", uID='" + uID + '\'' +
+                ", category=" + category +
+                '}';
+    }
+}
