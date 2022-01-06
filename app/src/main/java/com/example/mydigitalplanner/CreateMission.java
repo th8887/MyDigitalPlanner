e6e969da83e5b8a2b8be0c75c098a33d8e0b0ece
th8887 <th8887@bs.amalnet.k12.il>
2022-01-06 09:41:57 +0200

    corrections in MainActivity.

--- app/src/main/java/com/example/mydigitalplanner/CreateMission.java
+++ app/src/main/java/com/example/mydigitalplanner/CreateMission.java
@@ -2,13 +2,77 @@ package com.example.mydigitalplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class CreateMission extends AppCompatActivity {
+    FloatingActionButton fab;
+    /**
+     * cc- create category.
+     */
+    Button cc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_mission);
+        fab = findViewById(R.id.fab);
+        cc=(Button) findViewById(R.id.cc);
+
+        cc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    /**
+     * To Display the costum dialog.
+     */
+    void showCustomDialog(){
+        final Dialog dialog = new Dialog(CreateMission.this);
+        //We have added a title in the custom layout. So let's disable the default title.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog bu clicking anywhere outside the dialog.
+        dialog.setCancelable(true);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.custom_dialog);
+
+        //Initializing the views of the dialog.
+        final EditText nameC = dialog.findViewById(R.id.nameC);
+        Button add = dialog.findViewById(R.id.add);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameC.getText().toString();
+                //populateInfoTv(name,age,hasAccepted);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+    }
+
+
 }
